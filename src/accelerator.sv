aa71987194aa00f6ee6501b1070812c3c637d98d
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2019-10-10 13:07:01 +0200

    Add tcdm and configuration bus

--- src/accelerator.sv
+++ src/accelerator.sv
@@ -1,18 +1,26 @@
-module accelerator
-#(
-  parameter int unsigned N_CORES = 2,
-  parameter int unsigned MP  = 4,
-  parameter int unsigned ID  = 10
-)
-(
-  // global signals
-  input  logic                                  clk_i,
-  input  logic                                  rst_ni,
-  input  logic                                  test_mode_i,
+module accelerator #(
+  parameter int unsigned ID_WIDTH  = 0
+) (
+  input  logic          clk_i,
+  input  logic          rst_ni,
+  input  logic          test_mode_i,
 
-  output logic result
+  XBAR_TCDM_BUS.Master  tcdm_bus,
+  XBAR_PERIPH_BUS.Slave cfg_bus
 );
+
+  assign tcdm_bus.req     = '0;
+  assign tcdm_bus.add     = '0;
+  assign tcdm_bus.wen     = '0;
+  assign tcdm_bus.wdata   = '0;
+  assign tcdm_bus.be      = '0;
+
+  assign cfg_bus.gnt     = '0;
+  assign cfg_bus.r_rdata = '0;
+  assign cfg_bus.r_opc   = '0;
+  assign cfg_bus.r_id    = '0;
+  assign cfg_bus.r_valid = '0;
 
   assign result = ~clk_i;
 
-endmodule // mac_top
+endmodule
